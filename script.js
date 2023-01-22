83632aa873ecee4e4730257ee36dd0e96c9321a9
“thalytalucena” <thalucena9@gmail.com>
2023-01-22 00:31:30 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  const dayExist = nlwSetup.dayExists(today)
+
+  if (dayExist) {
+    alert("Dia já incluso!")
+    return
+  }
+
+  alert("Dia adicionado com sucesso!")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
